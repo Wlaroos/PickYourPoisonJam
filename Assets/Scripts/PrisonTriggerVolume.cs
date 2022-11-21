4b50bbb97b753a1c0cdf79919e5efab035cc3853
Ian Percy <ianedwardpercy@gmail.com>
2022-11-20 21:12:04 -0600

    Finished Prison Trigger
    -sorting layers work correctly
    -triggers move camera correctly
    -physics material makes objects slippery

--- Assets/Scripts/PrisonTriggerVolume.cs
+++ Assets/Scripts/PrisonTriggerVolume.cs
@@ -8,15 +8,33 @@ public class PrisonTriggerVolume : MonoBehaviour
     public bool frontTrigger;
     public bool behind = false;
 
+    private GameObject wallBroken;
+    private GameObject props;
+
+    public void Start(){
+        
+        props = GameObject.Find("Props");
+    }
+
     public void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
         
             if(behindTrigger){
+                if(wallBroken == null){
+                    wallBroken = GameObject.Find("WallBroken");
+                }
                 this.gameObject.transform.parent.gameObject.GetComponent<PrisonController>().BehindWall();   
+                wallBroken.GetComponent<SpriteRenderer>().sortingOrder = 2;
+                props.GetComponent<SpriteRenderer>().sortingOrder = 3;
             }
             
             else if(frontTrigger){
+                if(wallBroken == null){
+                    wallBroken = GameObject.Find("WallBroken");
+                }
                 this.gameObject.transform.parent.gameObject.GetComponent<PrisonController>().InFrontOfWall();
+                wallBroken.GetComponent<SpriteRenderer>().sortingOrder = -2;
+                props.GetComponent<SpriteRenderer>().sortingOrder = -1;
             }
         
         }
